9783adcab11c8a6391e98b886f0bb8daa3a951ae
astropatel <ri.patel272@gmail.com>
2024-11-25 22:37:21 -0800

    updating for sphinx readthedocs

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -1,28 +1,11 @@
-# Configuration file for the Sphinx documentation builder.
-#
-# This file only contains a selection of the most common options. For a full
+
 # list see the documentation:
-# https://www.sphinx-doc.org/en/master/usage/configuration.html
-
-# -- Path setup --------------------------------------------------------------
-
-# If extensions (or modules to document with autodoc) are in another directory,
-# add these directories to sys.path here. If the directory is relative to the
-# documentation root, use os.path.abspath to make it absolute, like shown here.
-#
 import os
 import sys
-# sys.path.append(os.path.abspath('../..'))
-# sys.path.append(os.path.abspath('../../twinkle'))
 sys.path.insert(0, os.path.abspath('../..'))
-# sys.path.insert(0, os.path.abspath('../../twinkle/'))
-# sys.path.insert(0, os.path.abspath('../../twinkle/utils'))
-
-# from twinkle import twinkle
 
 def setup(app):
     app.add_css_file('custom.css')
-
 
 nbsphinx_prolog = r"""
 .. raw:: html
@@ -45,11 +28,6 @@ release = '0.1'
 
 # -- General configuration ---------------------------------------------------
 
-# Add any Sphinx extension module names here, as strings. They can be
-# extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
-# ones.
-
-#mathjax_path="https://cdn.mathjax.org/mathjax/latest/MathJax.js?config=TeX-AMS-MML_HTMLorMML"
 mathjax_path = 'https://cdnjs.cloudflare.com/ajax/libs/mathjax/3.2.2/es5/tex-mml-chtml.js'
 
 numfig = True
@@ -67,8 +45,7 @@ extensions = ['sphinx.ext.autodoc',
               'sphinxcontrib.xlsxtable',
               'sphinx.ext.mathjax',
               'nbsphinx']
-# 'sphinx.ext.imgmath'
-# Add any paths that contain templates here, relative to this directory.
+
 templates_path = ['_templates']
 
 # List of patterns, relative to source directory, that match files and
@@ -78,15 +55,8 @@ exclude_patterns = []
 
 
 # -- Options for HTML output -------------------------------------------------
-
-# The theme to use for HTML and HTML Help pages.  See the documentation for
-# a list of builtin themes.
-#
 html_theme = 'sphinx_rtd_theme'
 
-# Add any paths that contain custom static files (such as style sheets) here,
-# relative to this directory. They are copied after the builtin static files,
-# so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
 
 html_css_files = ['custom.css']
