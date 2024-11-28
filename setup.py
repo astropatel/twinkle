c14a8102dcf3cdd0cec1c14086fff333999949e7
astropatel <ri.patel272@gmail.com>
2024-11-27 17:01:32 -0800

    fixed setup.py file to include packages

--- setup.py
+++ setup.py
@@ -9,15 +9,16 @@ with open('requirements.txt') as f:
 
 setup(
     name='twinkle',
-    # version='1.0.0',
+    version='1.0.0',
     description='A custom package for calculating stellar spectral energy distributions (SED) using broadband fluxes and grid models. Also has the capability to calculate excess flux.',
     author='Rahul I. Patel',
     url='https://github.com/astropatel/twinkle',
     author_email='ri.patel272@gmail.com',
     include_package_data=True,
-    packages=find_packages(where='twinkle'),
-    package_dir={"":"twinkle"},
+    packages=find_packages(),
+    #package_dir={"":"twinkle"},
     install_requires=required,
+    license='MIT',
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
